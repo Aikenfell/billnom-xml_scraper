0af5b6fc02883a37407fd2f88c6326af4b41fd06
andrew amakye ansah <eikeaaa15@gmail.com>
2023-01-28 18:20:30 -0600

    Scraper Minimum Completed

---
+++ XML_Scraper/scraper.py
@@ -0,0 +1,69 @@
+import requests
+import xml
+import lxml
+import json
+from bs4 import BeautifulSoup
+
+session = "0932023"
+session = "0932023"
+reqString = "https://www.revisor.mn.gov/bills/status_result.php?"
+reqString += "body=House&"
+reqString += "search=basic&"
+reqString += "session="+"&"
+reqString += "location=House&"
+reqString += "bill=&"
+reqString += "bill_type=bill&"
+reqString += "rev_number=&"
+reqString += "keyword_type=all&"
+reqString += "keyword=&"
+reqString += "keyword_field_text=1&"
+reqString += "author1%5B%5D=&"
+reqString += "author%5B%5D=&"
+reqString += "topic%5B%5D=1141&"
+
+reqString += "submit_topic=GO&"
+reqString += "committee%5B%5D=&"
+reqString += "action%5B%5D=&"
+reqString += "titleword=&"
+reqString += "format=xml"
+
+
+XMLresponse = requests.get(reqString)
+
+# with open("Stage1.xml", "w") as f:
+#     f.write(XMLresponse.text) 
+
+# with open('Stage1.xml', 'r') as f:
+#     XMLresponse = f.read()
+Stage2 = BeautifulSoup(XMLresponse.text, 'xml')
+
+retDict = {}
+
+for tag in Stage2.find_all('BILL_RESULT'):
+    BID = tag.find('FILE_TYPE').text+tag.find('FILE_NUMBER').text
+
+    tempDict = {}
+    # tempDict.update({"FILE_TYPE" : })
+    tempDict.update({"DESCRIPTION" : tag.find('DESCRIPTION').text})
+    tempDict.update({"BILL_URL" : tag.find('LATEST_TEXT_HTML_URI').text})
+
+    HTMLresponse = requests.get("https://"+tag.find('LATEST_TEXT_HTML_URI').text)
+    Stage4 = BeautifulSoup(HTMLresponse.text, 'lxml')
+    BillTex = ""
+    for tag in Stage4.find_all('ins'):
+        BillTex += tag.text+"\n"
+
+    tempDict.update({"BILL_TEXT" : BillTex})
+
+    retDict.update({BID : tempDict})
+
+
+# with open('Stage3.html', 'w') as f:
+#     f.write(HTMLresponse.text) 
+
+# with open('Stage3.html', 'r') as f:
+#     htmlresponse = f.read()
+
+
+with open("BillData.json", "w") as outfile:
+    json.dump(retDict, outfile, indent=4)
